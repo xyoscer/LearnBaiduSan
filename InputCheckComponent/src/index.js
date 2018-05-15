017ad5d9bf1d6fdd8aabebc56f147360ac258b6f
xygirl <1498546245@qq.com>
2018-05-15 16:06:54 +0800

    完成简单的组件封装

--- InputCheckComponent/src/index.js
+++ InputCheckComponent/src/index.js
@@ -2,6 +2,6 @@
  * Created by XiYin on 15/05/2018.
  */
 import {router} from 'san-router';
-import App from 'app.san';
+import App from './app.san';
 router.add({rule: '/',Component: App,target: '#app'});
 router.start();
